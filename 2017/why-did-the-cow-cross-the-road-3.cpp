bf09e78f8cbeb6a74e480d8a5d9ec9ee6b560184
Andrew Lou <andrewlou@Andrews-MacBook-Pro.local>
2025-07-02 14:54:55 -0700

    Input structure complete

---
+++ 2017/why-did-the-cow-cross-the-road-3.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <map>
+#include <utility>
+
+using namespace std;
+
+int N, K, R, distant_pairs = 0;
+map<pair<int, int>, vector<pair<int, int>>> adjMap;
+vector<vector<int>> grid;
+vector<vector<bool>> visited;
+
+int main() {
+    ifstream fin("countcross.in");
+    ofstream fout("countcross.out");
+
+    fin >> N >> K >> R;
+
+    grid.resize(N, vector<int>(N));
+    visited.resize(N, vector<bool>(N, false));
+
+
+    for (int i = 0; i < R; i++) {
+        int a, b, c, d;
+        fin >> a >> b >> c >> d;
+        adjMap[{a, b}].push_back({c, d});
+    }
+
+}
