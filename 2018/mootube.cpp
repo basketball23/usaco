816b7cc4a0a746d8377b7383010ef48df97d9a57
Andrew Lou <andrewloupro@gmail.com>
2025-07-03 14:42:37 -0700

    Completed first draft and basic structure

---
+++ 2018/mootube.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <utility>
+
+using namespace std;
+
+int N, Q, video_count = 0;
+vector<vector<pair<int, int>>> connections;
+vector<int> video_suggestions;
+vector<bool> visited;
+
+void dfs(int node, int current_depth, int min_relevance) {
+
+    visited[node] = true;
+    current_depth += i.second;
+
+    for (auto i : connections[node]) {
+        if (current_depth >= min_relevance) {
+            video_count++;
+        }
+        if (visited[i.first] == false) {
+            dfs(i.first, current_depth, min_relevance);
+        }
+
+    }
+}
+
+int main() {
+    ifstream fin("mootube.in");
+    ofstream fout("mootube.out");
+
+    fin >> N >> Q;
+
+    connections.resize(N, vector<pair<int, int>>(N));
+    video_suggestions.resize(Q, 0);
+    visited.resize(N, false);
+
+    for (int i = 0; i < N - 1; i++) {
+        int p, q, r;
+        cin >> p >> q >> r;
+
+        connections[p].push_back({q, r});
+        connections[q].push_back({p, r});
+    }
+
+    for (int i = 0; i < Q; i++) {
+        int k, v;
+        fin >> k >> v;
+        int relevance = k;
+
+        video_count = 0;
+        fill(visited.begin(), visited.end(), false);
+
+
+    }
+}
