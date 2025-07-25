0c564870d50a0999f78f43986001a98bba8acb86
Andrew Lou <andrewloupro@gmail.com>
2025-07-24 17:53:59 -0700

    Oh my days this actually worked, USACO guide had it marked under prefix sums, not greedy

--- codeforces/nusret-gokce.cpp
+++ codeforces/nusret-gokce.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main() {
+    // setup
     int n, m;
     cin >> n >> m;
 
     vector<int> steak_slices;
 
+    // input
     for (int i = 0; i < n; i++) {
         int salt;
         cin >> salt;
@@ -16,6 +19,7 @@ int main() {
         steak_slices.push_back(salt);
     }
 
+    // greedy, iterating forward
     for (int i = 0; i < n - 1; i++) {
         int a, b;
         a = steak_slices[i];
@@ -34,6 +38,9 @@ int main() {
         steak_slices[i + 1] = b;
     }
 
+    // reverse the vector and begin propagating from the opposite head
+    reverse(steak_slices.begin(), steak_slices.end());
+
     for (int i = 0; i < n - 1; i++) {
         int a, b;
         a = steak_slices[i];
@@ -52,9 +59,14 @@ int main() {
         steak_slices[i + 1] = b;
     }
 
+    // go back to normal
+    reverse(steak_slices.begin(), steak_slices.end());
+
+    // output
     for (int i = 0; i < n; i++) {
         cout << steak_slices[i] << " ";
     }
     cout << endl;
+
     return 0;
 }
