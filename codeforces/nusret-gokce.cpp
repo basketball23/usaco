a1d2deab2a60f2e667834603e3d21b1707813112
Andrew Lou <andrewloupro@gmail.com>
2025-07-24 17:39:35 -0700

    Greedy approach

--- codeforces/nusret-gokce.cpp
+++ codeforces/nusret-gokce.cpp
@@ -16,6 +16,42 @@ int main() {
         steak_slices.push_back(salt);
     }
 
+    for (int i = 0; i < n - 1; i++) {
+        int a, b;
+        a = steak_slices[i];
+        b = steak_slices[i + 1];
+
+        if (a > b) {
+            if (a - b > m) {
+                b += (a - b) - m;
+            }
+        } else {
+            if (b - a > m) {
+                a += (b - a) - m;
+            }
+        }
+        steak_slices[i] = a;
+        steak_slices[i + 1] = b;
+    }
+
+    for (int i = 0; i < n - 1; i++) {
+        int a, b;
+        a = steak_slices[i];
+        b = steak_slices[i + 1];
+
+        if (a > b) {
+            if (a - b > m) {
+                b += (a - b) - m;
+            }
+        } else {
+            if (b - a > m) {
+                a += (b - a) - m;
+            }
+        }
+        steak_slices[i] = a;
+        steak_slices[i + 1] = b;
+    }
+
     for (int i = 0; i < n; i++) {
         cout << steak_slices[i] << " ";
     }
