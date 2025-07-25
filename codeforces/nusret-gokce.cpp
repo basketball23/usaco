dbe35b41cba3b085e6c47160bda919ce67115717
Andrew Lou <andrewloupro@gmail.com>
2025-07-24 17:56:13 -0700

    Solved and code simplification

--- codeforces/nusret-gokce.cpp
+++ codeforces/nusret-gokce.cpp
@@ -19,48 +19,29 @@ int main() {
         steak_slices.push_back(salt);
     }
 
-    // greedy, iterating forward
-    for (int i = 0; i < n - 1; i++) {
-        int a, b;
-        a = steak_slices[i];
-        b = steak_slices[i + 1];
+    // greedy, iterating forward and backward to ensure the vector meets the requirements
 
-        if (a > b) {
-            if (a - b > m) {
-                b += (a - b) - m;
+    for (int j = 0; j < 2; j++) {
+        for (int i = 0; i < n - 1; i++) {
+            int a, b;
+            a = steak_slices[i];
+            b = steak_slices[i + 1];
+
+            if (a > b) {
+                if (a - b > m) {
+                    b += (a - b) - m;
+                }
+            } else {
+                if (b - a > m) {
+                    a += (b - a) - m;
+                }
             }
-        } else {
-            if (b - a > m) {
-                a += (b - a) - m;
-            }
+            steak_slices[i] = a;
+            steak_slices[i + 1] = b;
         }
-        steak_slices[i] = a;
-        steak_slices[i + 1] = b;
+        // flip the vector
+        reverse(steak_slices.begin(), steak_slices.end());
     }
-
-    // reverse the vector and begin propagating from the opposite head
-    reverse(steak_slices.begin(), steak_slices.end());
-
-    for (int i = 0; i < n - 1; i++) {
-        int a, b;
-        a = steak_slices[i];
-        b = steak_slices[i + 1];
-
-        if (a > b) {
-            if (a - b > m) {
-                b += (a - b) - m;
-            }
-        } else {
-            if (b - a > m) {
-                a += (b - a) - m;
-            }
-        }
-        steak_slices[i] = a;
-        steak_slices[i + 1] = b;
-    }
-
-    // go back to normal
-    reverse(steak_slices.begin(), steak_slices.end());
 
     // output
     for (int i = 0; i < n; i++) {
