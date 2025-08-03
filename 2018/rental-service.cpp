0ce6d53b3a75a3f83bf28b5bfeedd531d2911e04
Andrew Lou <andrewloupro@gmail.com>
2025-08-02 23:51:42 -0400

    Ai assisted solution, still too slow

--- 2018/rental-service.cpp
+++ 2018/rental-service.cpp
@@ -55,59 +55,38 @@ int main() {
         milk_prefix[i] = milk_prefix[i - 1] + milk[i];
     }
 
-    vector<long long> store_prefix(M + 1);
-    store_prefix[0] = 0;
-    for (int i = 1; i <= M; i++) {
-        // Milk the first i cows
-        // Store in total milk variable
+    vector<long long> milk_profit(N + 1);
+    int store_idx = 0;
+    int store_amt_left = stores[0].second;
 
-        long long milk_avaliable = milk_prefix[i - 1];
+    for (int i = 0; i < N; ++i) {
+        long long gallon = milk[i];
+        long long profit = 0;
 
-        // Fill store order as much as possible
-        // Save as cost
-        
-        // while milk is still avaliable, sell to top price per gallon store
-        int store_idx = 0;
-        int price = 0;
+        while (gallon > 0 && store_idx < M) {
+            int use = min((long long)store_amt_left, gallon);
+            profit += 1LL * use * stores[store_idx].first;
+            gallon -= use;
+            store_amt_left -= use;
 
-        while (milk_avaliable > 0) {
-            if (milk_avaliable > stores[store_idx].second) {
-                milk_avaliable -= stores[store_idx].second;
-
-                price += stores[store_idx].first * stores[store_idx].second;
-            } else {
-                price += stores[store_idx].first * milk_avaliable;
-                milk_avaliable = 0;
-                break;
+            if (store_amt_left == 0 && store_idx + 1 < M) {
+                store_idx++;
+                store_amt_left = stores[store_idx].second;
             }
-
-            store_idx++;
         }
-        
-        store_prefix[i] = price;
+
+        milk_profit[i + 1] = milk_profit[i] + profit;
     }
 
-    // for each i, calculate i and N - i for store and rental
     // max() to find if it's larger than currently
 
     long long current_profit;
 
-    int rental_cows;
-    if (R < N) {
-        rental_cows = R - 1;
-    } else {
-        rental_cows = N - 1;
-    }
+    for (int i = 0; i <= N; i++) {
+        int rent_cows = N - i;
+        rent_cows = min(rent_cows, R);
 
-    for (int i = 0; i < M; i++) {
-        if (i + rental_cows >= N) {
-            rental_cows -= 1;
-        }
-        if (rental_cows < 0) {
-            rental_cows = 0;
-        }
-        current_profit = store_prefix[i] + rental_prefix[rental_cows];
-        max_profit = max(max_profit, current_profit);
+        max_profit = max(max_profit, milk_profit[i] + rental_prefix[rent_cows]);
     }
 
     fout << max_profit << "\n";
