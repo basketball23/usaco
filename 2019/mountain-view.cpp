623a4f01615e66b73a729a9c1ae272938ee4621f
Andrew Lou <andrewloupro@gmail.com>
2025-08-03 00:30:14 -0400

    n^2 algorithm work in progress

--- 2019/mountain-view.cpp
+++ 2019/mountain-view.cpp
@@ -12,6 +12,8 @@ int main() {
     int N; fin >> N;
 
     vector<pair<int, int>> peaks;
+    vector<pair<int, int>> distinct;
+
     for (int i = 0; i < N; i++) {
         int x, y; fin >> x >> y;
         peaks.push_back({x, y});
@@ -23,7 +25,16 @@ int main() {
 
     reverse(peaks.begin(), peaks.end());
 
-    
+    distinct.push_back(peaks[0]);
+    for (int i = 1; i < N; i++) {
+        for (int j = 0; j < distinct.size(); j++) {
+            int x1 = peaks[1].first, y1 = peaks[i].second, x2 = distinct[j].first, y2 = distinct[j].second;
+            int xdiff = x2 - x1, ydiff = y2 - y1;
+            if (abs(xdiff) > ydiff) {
+                
+            }
+        }
+    }
 
     // sort by y values
     // iterate through, and for each coordinate you can calculate if it exists in another by comparing y values, taking the difference and applying to x vals
