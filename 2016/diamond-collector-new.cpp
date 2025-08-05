aeb36af0c13a6a42c06befc553858920e26560e3
Andrew Lou <andrewloupro@gmail.com>
2025-08-05 19:21:12 -0400

    Improved pass rate

--- 2016/diamond-collector-new.cpp
+++ 2016/diamond-collector-new.cpp
@@ -24,9 +24,13 @@ int main() {
 
     // two pointers, keep track of how many elements in largest and second largest possible subarrays. Sum, and get answer
 
-    long long first = 0, second = 0;
+    int first = 0, second = 0;
 
     int left = 0;
+
+    int l, r;
+
+    // inital pass
 
     for (int right = 0; right < N; right++) {
         while (diamonds[right] - diamonds[left] > K) {
@@ -34,10 +38,36 @@ int main() {
         }
 
         if (right - left + 1 > first) {
-            second = first;
             first = right - left + 1;
-        } 
+            l = left;
+            r = right;
+        }
+    }
 
+    left = 0;
+
+    // second pass to find second largest subarray, accounting for already used diamonds
+    
+    for (int right = 0; right < N; right++) {
+        while (diamonds[right] - diamonds[left] > K) {
+            left++;
+        }
+        
+        if (right >= l && right <= r) {
+            continue;
+        }
+
+        while (left >= l && left <= r) {
+            left++;
+        }
+        
+        if (right - left + 1 > second) {
+            if (right - left + 1 == first) {
+                continue;
+            } else {
+                second = right - left + 1;
+            }
+        }
     }
 
     fout << first + second << "\n";
