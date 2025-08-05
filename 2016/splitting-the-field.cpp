b3cd6f2f93357c9f6238c5bf2c1ce31caea53baf
Andrew Lou <andrewloupro@gmail.com>
2025-08-04 20:58:01 -0400

    FIRST GOLD PROBLEM SOLVED

--- 2016/splitting-the-field.cpp
+++ 2016/splitting-the-field.cpp
@@ -9,7 +9,6 @@ using namespace std;
 int main() {
     ifstream fin("split.in");
     ofstream fout("split.out");
-    
 
     int N; fin >> N;
     int xmax = 0, xmin = 1000000000, ymax = 0, ymin = 1000000000;
@@ -22,17 +21,17 @@ int main() {
         cows.push_back({x, y});
     }
 
-    long long single_enc_area = (xmax - xmin) * (ymax - ymin);
-    long long double_enc_area = 2147483647;
+    long long single_enc_area = 1LL * (xmax - xmin) * (ymax - ymin);
+    long long double_enc_area = 9223372036854775807;
 
-    // ALGORITHM:
+
     /*
     Sort points by x coordinate, and check bounding boxes by partitioning at each x-coordinate
     */
 
     sort(cows.begin(), cows.end());
 
-    //create list of min and max y at each x-coordinate
+    // Create list of min and max y at each x-coordinate
 
     vector<int> limit_list_left;
     ymax = 0, ymin = 1000000000;
@@ -63,22 +62,24 @@ int main() {
         left_r = cows[i].first;
         right_l = cows[i + 1].first;
 
-        long long left_area = (left_r - left_l) * (limit_list_left[i]);
-        long long right_area = (right_r - right_l) * (limit_list_right[i + 1]);
+        long long left_area = 1LL * (left_r - left_l) * (limit_list_left[i]);
+        long long right_area = 1LL * (right_r - right_l) * (limit_list_right[i + 1]);
 
         long long current_area = left_area + right_area;
 
         double_enc_area = min(double_enc_area, current_area);
     }
 
-    // TODO: run a horizontal loop
+
+
+    // Horizontal loop sorting and running by y values
 
     sort(cows.begin(), cows.end(), [](const pair<int, int>& a, const pair<int, int>& b) {
         return a.second < b.second;
     });
 
 
-    //create list of min and max x at each y-coordinate
+    // Create list of min and max x at each y-coordinate
 
     vector<int> limit_list_bottom;
     xmax = 0, xmin = 1000000000;
@@ -98,6 +99,7 @@ int main() {
 
     int bottom_b = cows[0].second, bottom_t, top_b, top_t = cows[N - 1].second;
 
+    // Horizontal processing loop
     for (int i = 0; i < N - 1; i++) {
         if (cows[i].second == cows[i + 1].second) {
             // skip processing y val if duplicates
@@ -106,8 +108,8 @@ int main() {
         bottom_t = cows[i].second;
         top_b = cows[i + 1].second;
 
-        long long bottom_area = (bottom_t - bottom_b) * (limit_list_bottom[i]);
-        long long top_area = (top_t - top_b) * (limit_list_top[i + 1]);
+        long long bottom_area = 1LL * (bottom_t - bottom_b) * (limit_list_bottom[i]);
+        long long top_area = 1LL * (top_t - top_b) * (limit_list_top[i + 1]);
 
         long long current_area = bottom_area + top_area;
 
