917ba82d0af9a0269249124e460fbf2251e49f4d
Andrew Lou <andrewloupro@gmail.com>
2025-08-24 09:18:49 -0700

    Created and updated angry cows file from offline work

---
+++ 2016/angry-cows-gold.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ifstream fin("angry.in");
+    ofstream fout("angry.out");
+
+    // Binary search for each possible 'R' value, range(0, largestx - minx)
+    // Try the 'R' value, if it works, try a lower 'R' value
+
+
+    // Array of possible R values will look like false false false false true true
+    // Our job is to find minimum true that makes the function valid.
+
+
+    // O(n log n) time complexity
+
+    
+}
