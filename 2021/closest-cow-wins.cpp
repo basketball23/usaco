4b101d08dceffd7cc82825e2e30f82ecde34bb8d
Andrew Lou <andrewloupro@gmail.com>
2025-09-10 08:05:15 -0700

    Official solution, using intervals and sliding window.
    Iterate across an array of nhoj's cows and grassy patches combined. If you hit a cow, iterate a sliding window between the cow and the next cow, maintaining a window of size interval_size/2 (This follows the rules).
    
    Then, add the maximum value you can achieve to an array, for both one and two cows.
    
    At the end, just take the top N cows.

--- 2021/closest-cow-wins.cpp
+++ 2021/closest-cow-wins.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <utility>
 #include <algorithm>
+#include <cassert>
 
 using namespace std;
 
@@ -10,61 +11,62 @@ int main() {
     int K, M, N;
     cin >> K >> M >> N;
 
-    // Algorithm:
-    /*
-    Iterate through each grassy patch (by order or tastiness or position?)
-
-    Determine the closest farmer Nhoj's cow using binary search
-
-    Test the farthest farmer John's cow can be while still winning it, to see if we can win more
-    Mark all won patches in a separate vector
-
-    Add total 
-    */
-
-    vector<vector<int>> patches_bysize;
-    vector<pair<int, int>> grassy_patches;
-    vector<bool> patches_won(K, false);
-
+    vector<pair<int, int>> patches(K + M);
     for (int i = 0; i < K; i++) {
-        int p, t;
-        cin >> p >> t;
-
-        patches_bysize.push_back({p, t, i});
-        grassy_patches.push_back({p, t});
+        cin >> patches[i].first >> patches[i].second;
     }
 
-    sort(patches_bysize.begin(), patches_bysize.end(), [](const vector<int>& a, const vector<int>& b) {
-        return a[1] > b[1];
+    for (int i = K; i < K + M; i++) {
+        cin >> patches[i].first;
+        patches[i].second = -1;
+    }
+
+    sort(patches.begin(), patches.end());
+
+    vector<unsigned long long> increases;
+    int last_i = -1;
+
+    unsigned long long sum_range = 0;
+
+    for (int i = 0; i < (int)patches.size(); ++i) {
+        if (patches[i].second == -1) {
+            if (last_i == -1) {
+                increases.push_back(sum_range);
+            } else {
+                unsigned long long cur_ans_1 = 0;
+                unsigned long long best_ans_1 = 0;
+                for (int j = last_i + 1, r = last_i; j < i; ++j) {
+                    while (r + 1 < i &&
+                        (patches[r + 1].first - patches[j].first) * 2 <
+                            patches[i].first - patches[last_i].first) {
+                        
+                        cur_ans_1 += patches[++r].second;
+                    }
+
+                    best_ans_1 = max(best_ans_1, cur_ans_1);
+                    cur_ans_1 -= patches[j].second;
+                }
+                assert(2 * best_ans_1 >= sum_range);
+
+                increases.push_back(best_ans_1);
+                increases.push_back(sum_range - best_ans_1);
+            }
+            last_i = i;
+            sum_range = 0;
+        } else {
+            sum_range += patches[i].second;
+        }
+    }
+
+    increases.push_back(sum_range);
+
+    sort(increases.begin(), increases.end(), [](const unsigned long long& a, const unsigned long long& b) {
+        return a > b;
     });
-
-
-    vector<int> nhoj_cows;
-    for (int i = 0; i < M; i++) {
-        int f;
-        cin >> f;
-
-        nhoj_cows.push_back(f);
+    increases.resize(N);
+    unsigned long long ans = 0;
+    for (auto val : increases) {
+        ans += val;
     }
-
-    sort(nhoj_cows.begin(), nhoj_cows.end());
-    // Main logic
-
-    for (int i = 0; i < K; i++) {
-        if (!patches_won[i]) {
-
-        }
-    }
-
-    // Counting ouput
-
-    long long total = 0;
-
-    for (int i = 0; i < K; i++) {
-        if (patches_won[i] == true) {
-            total += grassy_patches[i].second;
-        }
-    }
-
-    cout << total << "\n";
+    cout << ans << "\n";
 }
