317b274577bfc2dacaf15c0f38a23aa9e820c179
Andrew Lou <andrewloupro@gmail.com>
2025-09-14 13:55:31 -0700

    Added memoization

--- cses/graph-algorithms/flight-routes-check.cpp
+++ cses/graph-algorithms/flight-routes-check.cpp
@@ -20,15 +20,36 @@ int main() {
     }
 
     bool valid = true;
+    bool connected = false;
+    vector<int> values_that_work;
+
     for (int i = 0; i < n; i++) {
         queue<int> q;
         q.push(i);
         visited[i] = true;
+        connected = false;
 
         while (!q.empty()) {
             int curr_node = q.front();
             q.pop();
 
+            // can binary search
+
+            int lo = 0;
+            int hi = values_that_work.size();
+
+            while (lo < hi) {
+                int mid = lo + (hi - lo)/2;
+                if (values_that_work[mid] == curr_node) {
+                    connected = true;
+                    break;
+                } else if (values_that_work[mid] > curr_node) {
+                    hi = mid - 1;
+                } else {
+                    lo = mid + 1;
+                }
+            }
+            
             for (int node : adj[curr_node]) {
                 if (!visited[node]) {
                     visited[node] = true;
@@ -37,11 +58,16 @@ int main() {
             }
         }
 
+        if (connected) {
+            values_that_work.push_back(i);
+            fill(visited.begin(), visited.end(), false);
+            continue;
+        }
 
         for (int j = 0; j < n; j++) {
             if (visited[j] == false) {
                 cout << "NO" << "\n";
-                cout << j << " " << i << "\n";
+                cout << ++i << " " << ++j << "\n";
                 valid = false;
                 break;
             }
@@ -51,6 +77,7 @@ int main() {
             break;
         }
 
+        values_that_work.push_back(i);
         fill(visited.begin(), visited.end(), false);
     }
 
