c4cfecbfab6214890d064dc3bdd9961e687abba7
Andrew Lou <andrewloupro@gmail.com>
2025-09-14 14:13:49 -0700

    Solved!
    Used a clever solution where you only need 2 bfs runs: one to check if you can reach every city from city 1, and another to check if you can reach city 1 from every other city.

--- cses/graph-algorithms/flight-routes-check.cpp
+++ cses/graph-algorithms/flight-routes-check.cpp
@@ -5,11 +5,15 @@
 using namespace std;
 
 int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+
     int n, m;
     cin >> n >> m;
 
     vector<vector<int>> adj(n);
+    vector<vector<int>> radj(n);
     vector<bool> visited(n, false);
+    vector<bool> rvisited(n, false);
 
     for (int i = 0; i < m; i++) {
         int a, b;
@@ -17,69 +21,58 @@ int main() {
         a--; b--;
 
         adj[a].push_back(b);
+        radj[b].push_back(a);
+    }
+
+    // first bfs
+    queue<int> q;
+    q.push(0);
+    visited[0] = true;
+
+    while (!q.empty()) {
+        int curr_node = q.front();
+        q.pop();
+        
+        for (int node : adj[curr_node]) {
+            if (!visited[node]) {
+                visited[node] = true;
+                q.push(node);
+            }
+        }
+    }
+
+    queue<int> q2;
+    q2.push(0);
+    rvisited[0] = true;
+
+    while (!q2.empty()) {
+        int curr_node = q2.front();
+        q2.pop();
+        
+        for (int node : radj[curr_node]) {
+            if (!rvisited[node]) {
+                rvisited[node] = true;
+                q2.push(node);
+            }
+        }
     }
 
     bool valid = true;
-    bool connected = false;
-    vector<int> values_that_work;
-
-    for (int i = 0; i < n; i++) {
-        queue<int> q;
-        q.push(i);
-        visited[i] = true;
-        connected = false;
-
-        while (!q.empty()) {
-            int curr_node = q.front();
-            q.pop();
-
-            // can binary search
-
-            int lo = 0;
-            int hi = values_that_work.size();
-
-            while (lo < hi) {
-                int mid = lo + (hi - lo)/2;
-                if (values_that_work[mid] == curr_node) {
-                    connected = true;
-                    break;
-                } else if (values_that_work[mid] > curr_node) {
-                    hi = mid - 1;
-                } else {
-                    lo = mid + 1;
-                }
-            }
-            
-            for (int node : adj[curr_node]) {
-                if (!visited[node]) {
-                    visited[node] = true;
-                    q.push(node);
-                }
-            }
-        }
-
-        if (connected) {
-            values_that_work.push_back(i);
-            fill(visited.begin(), visited.end(), false);
-            continue;
-        }
-
-        for (int j = 0; j < n; j++) {
-            if (visited[j] == false) {
-                cout << "NO" << "\n";
-                cout << ++i << " " << ++j << "\n";
-                valid = false;
-                break;
-            }
-        }
-
-        if (!valid) {
+    for (int j = 0; j < n; j++) {
+        if (visited[j] == false) {
+            cout << "NO" << "\n";
+            cout << 1 << " " << ++j << "\n";
+            valid = false;
             break;
         }
-
-        values_that_work.push_back(i);
-        fill(visited.begin(), visited.end(), false);
+        if (rvisited[j] == false) {
+            cout << "NO" << "\n";
+            cout << ++j << " " << 1 << "\n";
+            valid = false;
+            break;
+        }
     }
+    
 
     if (valid) {
         cout << "YES" << "\n";
