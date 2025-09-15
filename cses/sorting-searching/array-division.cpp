dccdb1077a34cad2bc6b5b3fa9ce2ea1b0305390
Andrew Lou <andrewloupro@gmail.com>
2025-09-14 17:58:34 -0700

    Work in progress

--- cses/sorting-searching/array-division.cpp
+++ cses/sorting-searching/array-division.cpp
@@ -3,11 +3,39 @@
 
 using namespace std;
 
+vector<int> nums;
+
+bool canDivide(int max_sum) {
+
+}
+
 int main() {
     int n, k;
     cin >> n >> k;
 
+    nums.resize(n);
     for (int i = 0; i < n; i++) {
-        
+        int x;
+        cin >> x;
+
+        nums[i] = x;
     }
+
+    // find maximum minimum sum of array division
+    // function looks like (false, false, false, true, true, true)
+
+    int lo = 0;
+    int hi = nums.size() - 1;
+
+    while (lo < hi) {
+        int mid = lo + (hi - lo)/2;
+
+        if (canDivide(mid)) {
+            hi = mid;
+        } else {
+            lo = mid + 1;
+        }
+    }
+
+    cout << lo << "\n";
 }
