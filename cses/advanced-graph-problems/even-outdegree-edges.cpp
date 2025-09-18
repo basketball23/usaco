41186191225b3711aa08aa6b7824f88c99e785d7
Andrew Lou <andrewloupro@gmail.com>
2025-09-17 22:28:23 -0700

    Start of the problem

---
+++ cses/advanced-graph-problems/even-outdegree-edges.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+vector<vector<int>> adj;
+vector<bool> visited;
+
+void dfs(int node) {
+    visited[node] = true;
+    // more logic here
+
+    for (int child : adj[node]) {
+        if (!visited[child]) {
+            // add logic here
+
+            dfs(child);
+        }
+    }
+}
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+
+    int n, m;
+    cin >> n >> m;
+
+    adj.resize(n);
+    visited.resize(n);
+
+    for (int i = 0; i < m; i++) {
+        int a, b;
+        cin >> a >> b;
+        a--; b--;
+
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+
+    // Algo: use a greedy approach - if odd, flip one edge to make it even
+}
