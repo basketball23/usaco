5a9cf68406c89226f00af4541cfff5a39b4561b2
Andrew Lou <andrewloupro@gmail.com>
2025-09-27 15:25:16 -0700

    Work in progress

--- 2024/deforestation.cpp
+++ 2024/deforestation.cpp
@@ -32,11 +32,26 @@ int main() {
         for (int i = 0; i < K; i++) {
             cin >> restrictions[i].l >> restrictions[i].r >> restrictions[i].t;
         }
+        sort(restrictions.begin(), restrictions.end(), [](Restriction& a, Restriction& b){
+            if (a.r != b.r) {
+                return a.r < b.r;
+            } else {
+                return a.l < a.l;
+            }
+        });
 
         int total_cut = 0;
         int idx = 0;
+        priority_queue<Restriction, vector<Restriction>, greater<Restriction>> pq;
+        vector<int> kept;
+
+        for (int i = 0; i < N; i++) {
+            restrictions[i] 
+        }
+
+
         
-        while (idx < N) {
+        //while (idx < N) {
             /*
             ALGORITHM:
             iterate through each tree, and basically ASSIGN the tree to all avaliable restricitons
@@ -44,7 +59,7 @@ int main() {
 
             if there are no restrictions to fill when we have a tree, then increment a count (of trees able to cut down)
             */
-        }
+        //}
 
         cout << total_cut << "\n";
     }
