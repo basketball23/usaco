3a32b4a907d3b7c415d2883aa825ced9fb57a8a0
Andrew Lou <andrewloupro@gmail.com>
2025-09-27 17:46:41 -0700

    1st draft done
    Implemented an unordered_map + pq solution

--- 2017/milk-measurement.cpp
+++ 2017/milk-measurement.cpp
@@ -19,9 +19,6 @@ int main() {
     int N, G;
     fin >> N >> G;
 
-    int curr_wall_count = 10e9;
-    int adjustions = 0;
-
 
     vector<Measurement> measurements(N);
     for (int i = 0; i < N; i++) {fin >> measurements[i].day >> measurements[i].id >> measurements[i].chg;}
@@ -29,21 +26,47 @@ int main() {
         return a.day < b.day;
     });
 
+    int prev_wall_count = 10e9;
+    int curr_wall_count = 10e9;
+    int adjustions = 0;
 
     unordered_map<int, int> map;
-    priority_queue<int> pq;
     for (int i = 0; i < N; i++) {
+        priority_queue<int> pq;
         Measurement curr = measurements[i];
 
         if (map.find(curr.id) == map.end()) {
             map[curr.id] = G + curr.chg;
         }
-        pq.push(map[curr.id]);
+        
+        for (const auto& pair : map) {
+            pq.push(pair.second);
+        }
 
 
         int top = pq.top();
         if (top > G) {
-            
+            pq.pop();
+            curr_wall_count = 1;
+
+            while (pq.top() == top) {
+                curr_wall_count++;
+                pq.pop();
+            }
+        } else if (top == G) {
+            int salvage = 0;
+            while (pq.top() == G) {
+                salvage++;
+                pq.pop();
+            }
+
+            curr_wall_count = prev_wall_count - pq.size() + salvage;
+        } else {
+            curr_wall_count = prev_wall_count - pq.size();
+        }
+
+        if (curr_wall_count != prev_wall_count) {
+            adjustions++;
         }
     }
 
