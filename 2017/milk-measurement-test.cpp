bd804302e66d803d8a9eb16e4337ed559151aae3
Andrew Lou <andrewloupro@gmail.com>
2025-10-05 17:49:49 -0700

    New approach with test file

---
+++ 2017/milk-measurement-test.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <unordered_map>
+#include <queue>
+#include <algorithm>
+#include <set>
+#include <utility>
+
+using namespace std;
+
+struct Measurement {
+    int day;
+    int id;
+    int chg;
+};
+
+int main() {
+    ifstream fin("measurement.in");
+    ofstream fout("measurement.out");
+
+    int N, G;
+    fin >> N >> G;
+
+
+    vector<Measurement> measurements(N);
+    for (int i = 0; i < N; i++) {fin >> measurements[i].day >> measurements[i].id >> measurements[i].chg;}
+    sort(measurements.begin(), measurements.end(), [](Measurement& a, Measurement& b) {
+        return a.day < b.day;
+    });
+
+    int adjustions = 0;
+
+
+    unordered_map<int, int> map;
+    set<int> prev_on_wall;
+    set<int> prev_not_on;
+
+    for (int i = 0; i < N; i++) {
+        set<int> on_wall;
+        set<int> not_on;
+
+        priority_queue<pair<int, int>> pq;
+        Measurement curr = measurements[i];
+
+        if (map.find(curr.id) == map.end()) {
+            map[curr.id] = G + curr.chg;
+        } else {
+            map[curr.id] += curr.chg;
+        }
+        
+        for (const auto& pair : map) {
+            pq.push(make_pair(pair.second, pair.first));
+        }
+
+
+        pair<int, int> top = pq.top();
+        pq.pop();
+
+        if (top.first == G) {
+            while (pq.top().first == G) {
+                pq.pop();
+            }
+            while (!pq.empty()) {
+                not_on.insert(pq.top().second);
+                pq.pop();
+            }
+
+        } else if (top.first > G) {
+            on_wall.insert(top.second);
+            while (pq.top().first == top.first) {
+                on_wall.insert(pq.top().second);
+                pq.pop();
+            }
+        } else {
+            while (!pq.empty()) {
+                not_on.insert(pq.top().second);
+                pq.pop();
+            }
+        }
+
+
+        if (on_wall == prev_on_wall || not_on == prev_not_on) {
+            adjustions++;
+        }
+        
+        prev_on_wall = on_wall;
+        prev_not_on = not_on;
+    }
+
+
+    fout << adjustions << "\n";
+}
