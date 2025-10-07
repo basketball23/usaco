c5d5a7eef0064a9ebbdabd19473338a7bd40dd94
Andrew Lou <andrewloupro@gmail.com>
2025-10-06 19:33:51 -0700

    Solved! Implemented new approach and fixed index out-of-bounds issue

--- 2017/milk-measurement.cpp
+++ 2017/milk-measurement.cpp
@@ -3,6 +3,9 @@
 #include <vector>
 #include <unordered_map>
 #include <queue>
+#include <algorithm>
+#include <set>
+#include <utility>
 
 using namespace std;
 
@@ -13,6 +16,8 @@ struct Measurement {
 };
 
 int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+
     ifstream fin("measurement.in");
     ofstream fout("measurement.out");
 
@@ -21,54 +26,73 @@ int main() {
 
 
     vector<Measurement> measurements(N);
-    for (int i = 0; i < N; i++) {fin >> measurements[i].day >> measurements[i].id >> measurements[i].chg;}
+    for (int i = 0; i < N; i++) {
+        fin >> measurements[i].day >> measurements[i].id >> measurements[i].chg;
+    }
+
     sort(measurements.begin(), measurements.end(), [](Measurement& a, Measurement& b) {
         return a.day < b.day;
     });
 
-    int prev_wall_count = 10e9;
-    int curr_wall_count = 10e9;
     int adjustions = 0;
 
-    unordered_map<int, int> map;
+
+    unordered_map<int, int> milk;
+    set<int> prev_on_wall;
+    set<int> prev_not_on;
+
     for (int i = 0; i < N; i++) {
-        priority_queue<int> pq;
+        set<int> on_wall;
+        set<int> not_on;
+
+        priority_queue<pair<int, int>> pq;
         Measurement curr = measurements[i];
 
-        if (map.find(curr.id) == map.end()) {
-            map[curr.id] = G + curr.chg;
+        if (milk.find(curr.id) == milk.end()) {
+            milk[curr.id] = G + curr.chg;
+        } else {
+            milk[curr.id] += curr.chg;
         }
         
-        for (const auto& pair : map) {
-            pq.push(pair.second);
+        for (const auto& pair : milk) {
+            pq.push(make_pair(pair.second, pair.first));
         }
 
 
-        int top = pq.top();
-        if (top > G) {
-            pq.pop();
-            curr_wall_count = 1;
+        pair<int, int> top = pq.top();
+        pq.pop();
 
-            while (pq.top() == top) {
-                curr_wall_count++;
+        if (top.first == G) {
+            while (!pq.empty() && pq.top().first == G) {
                 pq.pop();
             }
-        } else if (top == G) {
-            int salvage = 0;
-            while (pq.top() == G) {
-                salvage++;
+            while (!pq.empty()) {
+                not_on.insert(pq.top().second);
                 pq.pop();
             }
 
-            curr_wall_count = prev_wall_count - pq.size() + salvage;
+        } else if (top.first > G) {
+            on_wall.insert(top.second);
+            while (!pq.empty() && pq.top().first == top.first) {
+                on_wall.insert(pq.top().second);
+                pq.pop();
+            }
         } else {
-            curr_wall_count = prev_wall_count - pq.size();
+            while (!pq.empty()) {
+                not_on.insert(pq.top().second);
+                pq.pop();
+            }
         }
 
-        if (curr_wall_count != prev_wall_count) {
+
+        if (on_wall != prev_on_wall || not_on != prev_not_on) {
             adjustions++;
         }
+        
+        prev_on_wall = on_wall;
+        prev_not_on = not_on;
     }
+
 
     fout << adjustions << "\n";
 }
