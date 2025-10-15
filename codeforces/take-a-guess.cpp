f997ddf035f4eb5c760bf7fd1c8d8c428eed6e85
Andrew Lou <andrewloupro@gmail.com>
2025-10-15 10:26:16 -0700

    Main idea

---
+++ codeforces/take-a-guess.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+
+    // Use AND and XOR operators to find sum of elements
+    // & bitwise AND
+    // | bitwise OR
+    // ^ or xor bitwise XOR
+}
