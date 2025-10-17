dab6e12150343f59585059cd23a7f4eb9d05f57a
Andrew Lou <andrewloupro@gmail.com>
2025-10-17 13:01:46 -0700

    Added comments

--- atcoder/xor-sigma-problem.cpp
+++ atcoder/xor-sigma-problem.cpp
@@ -12,17 +12,25 @@ int main() {
         cin >> a;
     }
 
+    // Accounts for overcounting
 	long long ans = -accumulate(v.begin(), v.end(), 0LL);
 
 	for (int i = 0; i < 30; i++) {
+        // Calculate bitwise
 		int s = 0;
 		vector<int> pref = {1, 0};
 
 		for (int a : v) {
+            // Extract current bit and xor with running prefix xor
+            // Gets you the xors up to this position
 			s ^= (a >> i) & 1;
 
+            // Find the number of prefixes up to current position that have a DIFFERING XOR
+            // Because subarray has xor 1 if prefix[r] is different from prefix[l - 1]
+            // Then multiply by 2^i to get its contribution to the answer
 			ans += pref[s ^ 1] * 1LL << i;
 
+            // Add to prefix xor totals
 			pref[s]++;
 		}
 	}
