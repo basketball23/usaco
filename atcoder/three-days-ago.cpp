50a894819d741b7b7685b4de38f921190bc63ac1
Andrew Lou <andrewloupro@gmail.com>
2025-10-17 14:46:00 -0700

    Works on first testcase

--- atcoder/three-days-ago.cpp
+++ atcoder/three-days-ago.cpp
@@ -16,7 +16,7 @@ int main() {
     // USE PROPERTY THAT TWO NUMBERS WILL XOR EACH OTHER OUT IF THEY'RE THE SAME
     int ans = 0;
     int s = 0;
-    vector<int> prefOdd = {1, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    vector<int> prefOdd = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
     vector<int> prefEven = {1, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
     for (int i = 0; i < S.size(); i++) {
@@ -25,12 +25,12 @@ int main() {
         s ^= digit;
 
         if ((i + 1) % 2 == 0) {
-            cout << "debug :" << "digit: " << digit << " " << prefEven[s] << " " << s << "\n";
+            //cout << "debug :" << "digit: " << digit << " " << prefEven[s] << " " << s << "\n";
             ans += prefEven[s];
 
             prefEven[s]++;
         } else {
-            cout << "debug :" << "digit: " << digit << " " << prefOdd[s] << " " << s << "\n";
+            //cout << "debug :" << "digit: " << digit << " " << prefOdd[s] << " " << s << "\n";
             ans += prefOdd[s];
 
             prefOdd[s]++;
