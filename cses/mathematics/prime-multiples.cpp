1620da4f36c1fef2ce216d165a4cf7cc82215371
Andrew Lou <andrewloupro@gmail.com>
2025-10-20 12:45:09 -0700

    Now passes testcase 5 as well

--- cses/mathematics/prime-multiples.cpp
+++ cses/mathematics/prime-multiples.cpp
@@ -7,26 +7,26 @@ using namespace std;
 int main() {
     cin.tie(nullptr)->sync_with_stdio(false);
 
-    long long n;
+    unsigned long long n;
     int k;
     cin >> n >> k;
 
-    vector<long long> primes(k);
+    vector<unsigned long long> primes(k);
     for (int i = 0; i < k; i++) {
         cin >> primes[i];
     }
 
-    long long ans = 0;
+    unsigned long long ans = 0;
 
     // First pass
     for (int i = 0; i < k; i++) {
-        long long multiples = n / primes[i];
+        unsigned long long multiples = n / primes[i];
         ans += multiples;
     }
 
     // Second pass to get rid of duplicates
     for (int i = 0; i < k; i++) {
-        long long duplicates = 0;
+        unsigned long long duplicates = 0;
         for (int j = i + 1; j < k; j++) {
             duplicates += n / (primes[i] * primes[j]);
         }
